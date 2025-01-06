7a117b3c8bf10c248d950d451b8dd603bc2dd4af
Nev Stokes <gitlab@nevstokes.com>
2025-01-06 21:05:51 +0000

    Attempt to create group

--- terraform/main.tf
+++ terraform/main.tf
@@ -11,3 +11,11 @@ terraform {
 provider "azuread" {
   use_oidc = true
 }
+
+data "azuread_client_config" "current" {}
+
+resource "azuread_group" "example" {
+  display_name     = "example"
+  owners           = [data.azuread_client_config.current.object_id]
+  security_enabled = true
+}
