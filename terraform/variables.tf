5c039d252fa1a8b8e84c58be254667e714e22130
Nev Stokes <gitlab@nevstokes.com>
2025-01-09 17:55:41 +0000

    Temporary remove validation

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -10,8 +10,8 @@ variable "user_group_assignments" {
   description = "Map of users and their groups"
   type        = map(set(string))
 
-  validation {
-    condition     = alltrue([for user, assignments in var.user_group_assignments : length(setsubtract(keys(var.groups), assignments)) == 0])
-    error_message = "Group not recognised"
-  }
+  # validation {
+  #   condition     = alltrue([for user, assignments in var.user_group_assignments : length(setsubtract(keys(var.groups), assignments)) == 0])
+  #   error_message = "Group not recognised"
+  # }
 }
