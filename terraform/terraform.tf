9ac8978e957e0ddc22fbf288721b332248ac4525
Nev Stokes <gitlab@nevstokes.com>
2025-06-08 00:16:50 +0100

    Try again

--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -20,11 +20,9 @@ terraform {
   }
 }
 
-data "aws_caller_identity" "current" {}
-
 locals {
-  aws_read_role_arn  = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/TerraformRead"
-  aws_write_role_arn = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/TerraformProvisioning"
+  aws_read_role_arn  = "arn:aws:iam::058264337777:role/TerraformRead"
+  aws_write_role_arn = "arn:aws:iam::058264337777:role/TerraformProvisioning"
 
   # role_arn = terraform.applying ? local.aws_write_role_arn : local.aws_read_role_arn
   role_arn = local.aws_write_role_arn
